61980a9f2dce968d6c9ee654b48e2d8e92e1fddc
Garret Jennings <gjennings@me.com>
2017-04-16 22:45:04 -0700

    Initial commit.

---
+++ ItemQualityAsText.lua
@@ -0,0 +1,227 @@
+local IQAT = {
+    name = "ItemQualityAsText",
+    qualityText = {
+        [0] = "Junk (Gray)",
+        [1] = "Normal (White)",
+        [2] = "Fine (Green)",
+        [3] = "Superior (Blue)",
+        [4] = "Epic (Purple)",
+        [5] = "Legendary (Gold)"
+    },
+    selectedItem = {},
+    clickedItem = nil,
+    popupItemLink = nil,
+    ItemLink = nil,
+}
+
+function IQAT:AddText(tooltip, itemLink, itemParent)
+
+    -- Check to make sure itemLink is not null
+    if itemLink then
+        -- Gets the Item Quality
+        itemQuality = GetItemLinkQuality(itemLink)
+
+        -- Checks Item Quality Text Exists for Returned Value
+        if IQAT.qualityText[itemQuality] then
+            -- Edits Tooltip
+            tooltip:AddVerticalPadding(10)
+            tooltip:AddLine(IQAT.qualityText[itemQuality], "ZoFontGame", 1, 1, 1, CENTER, MODIFY_TEXT_TYPE_NONE, LEFT, false)
+
+            -- Use these to debug the item's parent
+            --tooltipText = string.format("%s\n%s", itemParent, IQAT.qualityText[itemQuality])
+            --tooltip:AddLine(tooltipText, "ZoFontGame", 1, 1, 1, CENTER, MODIFY_TEXT_TYPE_NONE, LEFT, false)
+        end
+    end
+end
+
+function IQAT:ShowTooltip(control)
+    -- Get Mouse Over Function
+    local mouseOverControl = moc()
+
+    -- Check Control is Item tooltip
+    if control == ItemTooltip then
+
+        -- Quest Rewards
+        if mouseOverControl:GetParent():GetName() == "ZO_InteractWindowRewardArea" then
+            local Index = mouseOverControl.index
+            IQAT.ItemLink = GetQuestRewardItemLink(Index)
+        end
+    end
+end
+
+function IQAT:UpdateTooltip(item, tooltip)
+    -- Set Variables
+    local itemLink = nil
+
+    -- Check item and selectedItem
+    if not item or not item.dataEntry or not item.dataEntry.data then
+        return
+    end
+
+    -- Check to make sure we haven't already edited the tool tip for this item. Without this duplicate lines are created
+    if self.selectedItem[tooltip] == item then
+        return
+    end
+
+    -- Sets Current Item as SelectedItem - This stops duplication on mouse over
+    self.selectedItem[tooltip] = item
+
+    -- Get Item Link if Tooltip
+    itemLink = self:GetItemLink(item)
+
+    -- Set Item Link as Global Variable (This is for comparison Tooltips)
+    self.ItemLink = itemLink
+
+    --Add Text
+    self:AddText(tooltip, itemLink, item:GetParent():GetName())
+end
+
+function IQAT:PopupTooltip(tooltip)
+    -- Variables
+    local itemLink = nil
+
+    -- Check to make sure we haven't already edited the tool tip for this item. Without this duplicate lines are created
+    if self.clickedItem == IQAT.popupItemLink  then
+        return
+    end
+
+    -- Sets ClickedItem Variable - This stops duplication
+    self.clickedItem = IQAT.popupItemLink
+
+    -- Get ItemLink
+    itemLink = self.popupItemLink
+
+    -- Check ItemLink
+    if itemLink then
+        self:AddText(tooltip, itemLink)
+    end
+end
+
+function IQAT:HideTooltip(tooltip)
+    -- Clear out variables
+    if tooltip == ItemTooltip then
+        self.selectedItem[tooltip] = nil
+        self.ItemLink = nil
+    elseif tooltip == PopupTooltip then
+        self.clickedItem = nil
+    end
+end
+
+function IQAT:GetItemLink(item)
+
+    -- Checks Item Exists
+    if item or item.GetParent then
+        -- Get Item Parent
+        local parent = item:GetParent()
+
+        -- Check Parent Exists
+        if parent then
+            -- Get Parent Name
+            local parentName = parent:GetName()
+
+            -- If Item is in Backpack -- Obsolete
+            --if parentName.find(parentName, "BackpackContents") then
+            --  return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            --end
+
+            -- If Item is in Player Inventory
+            if parentName == "ZO_PlayerInventoryListContents" then
+                return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+
+            -- If Item is in Quickslot
+            if parentName == "ZO_QuickSlotListContents" then
+                return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+
+            -- If Item is in Wallet - WTF is wallet?
+            if parentName == "ZO_InventoryWalletListContents" then
+                return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+
+            -- If Item is in Deconstruction Window
+            if parentName == "ZO_SmithingTopLevelDeconstructionPanelInventoryBackpackContents" then
+                return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+
+            -- If Item is in Improvement Window
+            if parentName == "ZO_SmithingTopLevelImprovementPanelInventoryBackpackContents" then
+                return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+
+            -- If Item is in Bank
+            if parentName == "ZO_PlayerBankBackpackContents" then
+                return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+
+            -- If Item is in Store
+            if parentName == "ZO_StoreWindowListContents" then
+                return GetStoreItemLink(item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+
+            -- If Item is in Guild Store
+            if parentName == "ZO_TradingHouseItemPaneSearchResultsContents" then
+                -- Check to see if Item is still Active
+                if item.dataEntry.data.timeRemaining ~= nil then
+                    if item.dataEntry.data.timeRemaining > 0 then
+                        return GetTradingHouseSearchResultItemLink(item.dataEntry.data.slotIndex)
+                    else
+                        return nil
+                    end
+                end
+            end
+
+            -- If Item is in Guild Store Listed Items
+            if parentName == "ZO_TradingHousePostedItemsListContents" then
+                return GetTradingHouseListingItemLink(item.dataEntry.data.slotIndex)
+            end
+
+            -- If Item is in Store Buy Back List
+            if parentName == "ZO_BuyBackListContents" then
+                return GetBuybackItemLink(item.dataEntry.data.slotIndex)
+            end
+
+            if parentName == "ZO_BuyBackListContents" then
+                return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+            end
+            --
+            -- If Parent is none of the Above
+            --return nil
+
+            -- Uncomment line below to show parents
+            --return GetItemLink(item.dataEntry.data.bagId, item.dataEntry.data.slotIndex, LINK_STYLE_DEFAULT)
+        else
+            return nil
+        end
+    else
+        return nil
+    end
+end
+
+function IQAT:OnLoad(event, name)
+    if(name == IQAT.name) then
+        -- Remove onLoad Event
+        EVENT_MANAGER:UnregisterForEvent(IQAT.name, EVENT_ADD_ON_LOADED)
+
+        -- Item Tooltip Hooks
+        ZO_PreHookHandler(ItemTooltip, "OnUpdate", function() self:UpdateTooltip(moc(), ItemTooltip) end)
+        ZO_PreHookHandler(ItemTooltip, "OnHide", function() self:HideTooltip(ItemTooltip) end)
+
+        -- Popup Tooltip Hooks
+        ZO_PreHookHandler(PopupTooltip, "OnUpdate", function() self:PopupTooltip(PopupTooltip) end)
+        ZO_PreHookHandler(PopupTooltip, "OnHide", function() self:HideTooltip(PopupTooltip) end)
+
+        -- Compare Tooltip 1
+        --ZO_PreHookHandler(ComparativeTooltip1, 'OnUpdate',    function () self:CompareToolTip(ComparativeTooltip1) end)
+        --ZO_PreHookHandler(ComparativeTooltip1, 'OnHide',  function () self:HideTooltip() end)
+
+        -- Compare Tooltip2
+        --ZO_PreHookHandler(ComparativeTooltip2, 'OnUpdate',    function () self:CompareToolTip(ComparativeTooltip2) end)
+        --ZO_PreHookHandler(ComparativeTooltip2, 'OnHide',  function () self:HideTooltip() end)
+
+        -- Handler for Item Clicks in Chat
+        LINK_HANDLER:RegisterCallback(LINK_HANDLER.LINK_CLICKED_EVENT, function(ItemLink, ...) IQAT.popupItemLink = ItemLink end)
+    end
+end
+
+EVENT_MANAGER:RegisterForEvent(IQAT.name, EVENT_ADD_ON_LOADED, function(...) IQAT:OnLoad(...) end)
---
+++ README.md
@@ -0,0 +1,42 @@
+# ItemQualityAsText
+
+This addon was originally created by *Cavalier07* and posted to [ESOUI](http://www.esoui.com/downloads/info745-ItemQualityasText.html).
+
+As a colorblind player, I have updated the addon to restore functionality following game and API updates.
+
+The below is a copy of the original description for prosperity.
+
+---
+
+Item Quality as Text shows you the quality and color of an item as text in an items Tool Tip.
+
+This addon is aimed at those who are visually impaired, color blind or just want to see text instead of trying to work out a color.
+
+What this Addon Does:
+
+Displays a text message showing the items quality and quality color in the tooltip when looking at items via:
+- Bag
+- Bank
+- Crafting (Refine, Deconstruct, Improvement Screens)
+- Guild Bank
+- Guild Stores
+- Guild Traders
+- NPC Stores
+
+This addon also displays a text message showing the items quality and quality color in the:
+- Popup Windows when clicking on an item from the chat box
+
+To Do List (In no particular order):
+- Add quality text to the Repair Tab in NPC Stores
+- Add quality text to items being compared
+- Add quality text to items that you are wearing
+- Add quality text to crafting create and research item tool tips
+- Add option to show text as plain white or as the color of the item (EG: Purple for Purple Items)
+- Item Quality on Quest Rewards
+
+Thanks to:
+The authors of the following addons whose code has helped this addon progress:
+
+- Data Daedra
+- Price Tracker
+
